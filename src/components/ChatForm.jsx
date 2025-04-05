f2726199f626a3254be258df48ac16a96ee267b8
bilal-ahmedd <bilalahmed35100@gmail.com>
2025-04-05 07:12:00 +0500

    Handling for submission and showing dynamic message in the chatbox

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const ChatForm = () => {
+
+const ChatForm = ({setChatHistory}) => {
+  const inputRef = useRef()
+  const handleFormSubmit = (e)=> {
+  
+    e.preventDefault();
+    const userMessage = inputRef.current.value.trim();
+    if( !userMessage ) return;
+    inputRef.current.value = "";
+    
+    //Update The chat history with the user Message
+    setChatHistory( history => [...history, { role: "user", text: userMessage} ] )
+  }
+
   return (
-    <form action="#" className="chat-form">
-        <input type="text" placeholder="Message..." className="message-input" required />
+    <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
+        <input ref={inputRef} type="text" placeholder="Message..." className="message-input" required />
         <button className="material-symbols-rounded"> keyboard_arrow_up </button>
     </form>
 )
